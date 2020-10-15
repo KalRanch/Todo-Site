88d4db2eee512362927720f43e80a91b30c8c380
KalRanch <bkbrinton@gmail.com>
2020-10-15 16:31:38 -0600

    date.js module created
    date.js module created to aid in the implementation of showing the date in list.ejs. Also took the time to make app.js smaller to help with load speed.

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-var items = [];
-var workItems = [];
+const items = [];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -14,28 +15,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-  };
-
-  var day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
 
   res.render("list", { listTitle: day, newItems: items });
 });
 
 app.post("/", function (req, res) {
-
   let item = req.body.newItems;
 
   if (req.body.list === "Work") {
-    workItems.push(item)
-    res.redirect("/work")
+    workItems.push(item);
+    res.redirect("/work");
   } else {
     items.push(item);
-    res.redirect('/')
+    res.redirect("/");
   }
 
   items.push(item);
@@ -47,17 +40,17 @@ app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newItems: workItems });
 });
 
-app.post('/work', function (req, res) {
-  let item = req.body.newItems
+app.post("/work", function (req, res) {
+  let item = req.body.newItems;
 
-  workItems.push(item)
+  workItems.push(item);
 
-  res.redirect("/work")
-})
+  res.redirect("/work");
+});
 
-app.get('/about', function (req, res) {
-  res.render('about')
-})
+app.get("/about", function (req, res) {
+  res.render("about");
+});
 
 app.listen(3001, function () {
   console.log("Server started on port 3001.");
