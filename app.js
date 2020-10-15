193f6c87afc9479314605e963ef12345e948f762
KalRanch <bkbrinton@gmail.com>
2020-10-15 15:57:22 -0600

    EJS pages added, app.js edited
    about.ejs created,
    footer.ejs created,
    header.ejs created,
    
    app.js edited so the ejs files are funtional.
    
    list.ejs was modularized so that the footer and header are split into new files for easier implementation.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 var items = [];
+var workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -22,16 +23,41 @@ app.get("/", function (req, res) {
 
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newItems: items });
+  res.render("list", { listTitle: day, newItems: items });
 });
 
 app.post("/", function (req, res) {
-  item = req.body.newItems;
+
+  let item = req.body.newItems;
+
+  if (req.body.list === "Work") {
+    workItems.push(item)
+    res.redirect("/work")
+  } else {
+    items.push(item);
+    res.redirect('/')
+  }
 
   items.push(item);
 
   res.redirect("/");
 });
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newItems: workItems });
+});
+
+app.post('/work', function (req, res) {
+  let item = req.body.newItems
+
+  workItems.push(item)
+
+  res.redirect("/work")
+})
+
+app.get('/about', function (req, res) {
+  res.render('about')
+})
 
 app.listen(3001, function () {
   console.log("Server started on port 3001.");
